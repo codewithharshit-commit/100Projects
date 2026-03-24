59cfd86987483442d49de3237a1fce04ec6ef6b6
Harshit <sharma.harshit.0008@gmail.com>
2026-03-24 22:10:26 +0530

    complete palette generator project

--- color-palette-generator/script.js
+++ color-palette-generator/script.js
@@ -3,6 +3,39 @@ const paletteContainer = document.querySelector(".palette-container");
 
 generateButton.addEventListener("click", generatePalette);
 
+paletteContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("copy-btn")) {
+    const hexValue = e.target.previousElementSibling.textContent;
+
+    navigator.clipboard
+      .writeText(hexValue)
+      .then(() => showCopySuccess(e.target));
+  } else if (e.target.classList.contains("color")) {
+    const hexValue =
+      e.target.nextElementSibling.querySelector(".hex-value").textContent;
+
+    navigator.clipboard
+      .writeText(hexValue)
+      .then(() =>
+        showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn")),
+      );
+  }
+});
+
+function showCopySuccess(element) {
+  element.classList.remove("far", "fa-copy");
+  element.classList.add("fas", "fa-check");
+
+  element.style.color = "#48bb78";
+
+  setTimeout(() => {
+    element.classList.remove("fas", "fa-check");
+    element.classList.add("far", "fa-copy");
+
+    element.style.color = "";
+  }, 1500);
+}
+
 function generatePalette() {
   const colors = [];
   for (let i = 0; i < 5; i++) {
