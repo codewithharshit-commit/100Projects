e258c5ea57d3492dc4d4dbb6f754e6e4862664f1
Harshit <sharma.harshit.0008@gmail.com>
2026-03-31 14:45:12 +0530

    started a location finder react project

--- location-finder/src/App.jsx
+++ location-finder/src/App.jsx
@@ -1,121 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Map, { Marker } from "react-map-gl/mapbox";
+import { useEffect, useState } from "react";
+import "./index.css";
+import { FlyToInterpolator } from "@deck.gl/core";
+// import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [viewState, setState] = useState({
+    latitude: 28.61,
+    longitude: 77.23,
+    zoom: 15,
+  });
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setState({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+          zoom: 15,
+          transitionDuration: 2000,
+          transitionInterpolator: new FlyToInterpolator(),
+        });
+      },
+      () => {
+        setState({
+          latitude: 28.61,
+          longitude: 77.23,
+          zoom: 15,
+        });
+      },
+    );
+  }, []);
+
+  if (!viewState) return <div>Loading map...</div>;
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+      <div className="w-screen h-screen">
+        <Map
+          {...viewState}
+          mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          onMove={(e) => {
+            if (e?.viewState) {
+              setState(e.viewState);
+            }
+          }}
         >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          <Marker latitude={viewState.latitude} longitude={viewState.longitude}>
+            ❌
+          </Marker>
+        </Map>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
